1c70f1dbec8ed8670e3ddfd33b280765518cef3e
Manuel Martinez <vmmf08@gmail.com>
2017-09-05 10:19:34 -0500

    Implement integration tests for inbound returnsç

--- spec/newgistics/inbound_return_spec.rb
+++ spec/newgistics/inbound_return_spec.rb
@@ -2,8 +2,21 @@ require 'spec_helper'
 
 RSpec.describe Newgistics::InboundReturn do
   describe '#save' do
-    context "when the inbound return is placed successfully" do
-      it 'updates the inbound_return object with the shipment_id and any errors or warnings'
+    vcr_options = { cassette_name: 'inbound_return/save/success' }
+    context "when the inbound return is placed successfully", vcr: vcr_options do
+      it "returns true" do
+        inbound_return = Newgistics::InboundReturn.new(return_attributes)
+
+        expect(inbound_return.save).to be(true)
+      end
+
+      it 'sets the id and any errors or warnings' do
+        inbound_return = Newgistics::InboundReturn.new(return_attributes)
+
+        inbound_return.save
+
+        expect(inbound_return.id).to eq('1754251')
+      end
     end
 
     vcr_options = { cassette_name: 'inbound_return/save/failure' }
@@ -26,10 +39,25 @@ RSpec.describe Newgistics::InboundReturn do
 
         expect(success).to eq false
         expect(inbound_return.errors.size).to eq 1
-        expect(inbound_return.errors.first).to eq( 
+        expect(inbound_return.errors.first).to eq(
           "Unable to create inbound return; Product SKU BCD-123 was not found"
         )
       end
     end
+  end
+
+  def return_attributes(overrides = {})
+    {
+      shipment_id: '91955463',
+      rma: 'RMA_NUMBER',
+      comments: 'Sample Comment',
+      items: [
+        {
+          sku: '1007-201-G',
+          qty: 1,
+          reason: 'Too Big'
+        }
+      ]
+    }.merge(overrides)
   end
 end
