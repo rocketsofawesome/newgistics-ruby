0a34cfe2323a6fae07093b2db1610cf4eadbdc33
Manuel Martinez <vmmf08@gmail.com>
2017-09-05 09:48:50 -0500

    Add specs to inventory details endpoint

--- spec/newgistics/order_spec.rb
+++ spec/newgistics/order_spec.rb
@@ -5,15 +5,14 @@ RSpec.describe Newgistics::Order do
     vcr_options = { cassette_name: 'order/save/successfully' }
     context "when the order is placed successfully", vcr: vcr_options do
       it 'updates the order object with the shipment_id and any errors or warnings' do
-        Newgistics.configure { |c| c.api_key = 'ABC123' }
         order = described_class.new(order_attributes)
-          
+
         order.save
 
         expect(order.shipment_id).not_to be_nil
       end
 
-      it "returns true" do 
+      it "returns true" do
         order = described_class.new(order_attributes)
 
         expect(order.save).to be(true)
@@ -22,14 +21,14 @@ RSpec.describe Newgistics::Order do
 
     vcr_options = { cassette_name: 'order/save/failure' }
     context "when placing the order fails", vcr: vcr_options do
-      it "returns false" do 
+      it "returns false" do
         Newgistics.configuration.api_key = nil
         order = described_class.new(order_attributes)
 
         expect(order.save).to be(false)
       end
 
-      it "saves the errors on the order" do 
+      it "saves the errors on the order" do
         Newgistics.configuration.api_key = nil
         order = described_class.new(order_attributes)
 
