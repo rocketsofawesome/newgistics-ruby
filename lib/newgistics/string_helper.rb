6c9f8b2296f7b5b3b1a678c5a36461702070d8a6
Manuel Martinez <vmmf08@gmail.com>
2018-09-21 12:47:02 -0500

    Reduce the number of objects allocated for request processing (#33)
    With this change we'll update our StringHelper class to perform
    in-place modifications to strings so we reduce the number of
    strings allocated when these methods get called.
    
    Sadly, readability will be affected by this change but I believe
    the positive impact this will have on memory usage is worth it,
    especially for large requests.

--- lib/newgistics/string_helper.rb
+++ lib/newgistics/string_helper.rb
@@ -1,13 +1,21 @@
 module Newgistics
   class StringHelper
+    CAMEL_CASED_STRING_REGEX = /([a-z])([A-Z])/
+    UNDERSCORED_STRING_REGEX = /([A-Za-z\d]+)(_|\z)/
+    CAPITALIZED_STRING_REGEX = /\A[A-Z]/
+
     def self.camelize(string, upcase_first: true)
-      result = string.to_s.gsub(/([a-z\d]+)(_|\z)/) { $1.capitalize }
-      result = result.gsub(/\A[A-Z]/) { $&.downcase } unless upcase_first
-      result
+      string.dup.to_s.tap do |s|
+        s.gsub!(UNDERSCORED_STRING_REGEX) { $1.capitalize! || $1 }
+        s.gsub!(CAPITALIZED_STRING_REGEX) { $&.downcase! } unless upcase_first
+      end
     end
 
     def self.underscore(string)
-      string.to_s.gsub(/([a-z])([A-Z])/) { "#{$1}_#{$2}" }.downcase
+      string.dup.to_s.tap do |s|
+        s.gsub!(CAMEL_CASED_STRING_REGEX) { "#{$1}_#{$2}" }
+        s.downcase!
+      end
     end
   end
 end
