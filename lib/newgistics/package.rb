b281630910448596798ed8afbe699c96af369ca7
Robert Brodie <robert@sumoheavy.com>
2018-12-19 12:49:54 -0500

    #38 Added package support (#39)
    * #38 Added package support
    
    * Split out PackageItem to its own model
    
    * Updated PackageItem and model and spec to look for capital I
    
    * Bumped version to 2.3.0
    
    * Reverted version change
    
    * Fixed version

---
+++ lib/newgistics/package.rb
@@ -0,0 +1,13 @@
+module Newgistics
+  class Package
+    include Newgistics::Model
+
+    attribute :tracking_number, String
+    attribute :weight, Float
+    attribute :billable_weight, Float
+    attribute :height, Float
+    attribute :width, Float
+    attribute :depth, Float
+    attribute :items, Array[PackageItem]
+  end
+end
